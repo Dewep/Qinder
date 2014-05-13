8e48a621d00ae1227d81b6b35b0487da809dbaca
Maigret Aurélien <dewep.net@gmail.com>
2014-05-13 18:59:05 +0200

    Verifs supplementaires sur les preferences

--- Library/fr/qinder/pref/Preferences.java
+++ Library/fr/qinder/pref/Preferences.java
@@ -6,31 +6,35 @@ import android.content.SharedPreferences;
 public class Preferences {
 	public static boolean exist(Context context, String namespace, String key) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
-		return pref.contains(key);
+		return pref == null ? false : pref.contains(key);
 	}
 
 	/* String */
 	public static String get(Context context, String namespace, String key, String defValue) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
-		return pref.getString(key, defValue);
+		return pref == null ? defValue : pref.getString(key, defValue);
 	}
 
 	/* Int */
 	public static int get(Context context, String namespace, String key, int defValue) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
-		return pref.getInt(key, defValue);
+		return pref == null ? defValue : pref.getInt(key, defValue);
 	}
 
 	/* Boolean */
 	public static boolean get(Context context, String namespace, String key, boolean defValue) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
-		return pref.getBoolean(key, defValue);
+		return pref == null ? defValue : pref.getBoolean(key, defValue);
 	}
 
 	/* String */
 	public static void set(Context context, String namespace, String key, String value) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
+		if (pref == null)
+			return;
 		SharedPreferences.Editor editor = pref.edit();
+		if (editor == null)
+			return;
 		editor.putString(key, value);
 		editor.commit();
 	}
@@ -38,7 +42,11 @@ public class Preferences {
 	/* Int */
 	public static void set(Context context, String namespace, String key, int value) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
+		if (pref == null)
+			return;
 		SharedPreferences.Editor editor = pref.edit();
+		if (editor == null)
+			return;
 		editor.putInt(key, value);
 		editor.commit();
 	}
@@ -46,7 +54,11 @@ public class Preferences {
 	/* Boolean */
 	public static void set(Context context, String namespace, String key, boolean value) {
 		SharedPreferences pref = context.getSharedPreferences(namespace, Context.MODE_PRIVATE);
+		if (pref == null)
+			return;
 		SharedPreferences.Editor editor = pref.edit();
+		if (editor == null)
+			return;
 		editor.putBoolean(key, value);
 		editor.commit();
 	}
